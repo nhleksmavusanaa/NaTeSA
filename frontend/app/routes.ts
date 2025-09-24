6737ff80ae561acd71db0bd52044bfa2834965d1
nhleksmavusanaa <87915372+nhleksmavusanaa@users.noreply.github.com>
2025-09-24 08:51:39 +0200

    Delete frontend directory

--- frontend/app/routes.ts
+++
@@ -1,9 +0,0 @@
-import { type RouteConfig, index, route } from "@react-router/dev/routes";
-
-export default [
-  index("routes/home.tsx"),             // Home page (/)
-  route("about", "routes/about.tsx"),   // /about
-  route("login", "routes/login.tsx"),   // /login
-  //route("register", "routes/register.tsx"), // /register
-//  route("dashboard", "routes/dashboard.tsx"), // /dashboard nhlanhla
-] satisfies RouteConfig;
